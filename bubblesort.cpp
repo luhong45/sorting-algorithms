b83b572d7420448d59545474bb452243629c4673
tony <f0952747529@gmail.com>
2025-11-28 15:50:09 +0800

    add .gitignore / tasks.json / bubblesort.cpp

---
+++ bubblesort.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+using namespace std;
+
+void bubblesort(int *arr, int len){
+    for(int i = 0; i < len - 1; i++){
+        for(int j = 0; j < len - 1 - i; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+void printarray(int *arr, int len){
+    for(int i = 0; i < len; i++){
+        printf("%d ", arr[i]);
+    }
+}
+int main(){
+    int array[] = {3, 5, 8, 10, 2, 15, 1};
+    int len = sizeof(array) / sizeof(array[0]);
+    bubblesort(array, len);
+    printarray(array, len);
+}
